ede44ad0a4816a5c86112dd7d8d05704c50ae7db
Alberth Montero <alberthm@gmail.com>
2014-05-06 15:34:32 +0200

    Added Protocol name for Goal 12

--- group4/src/main/java/hotelmania/group4/HotelManiaAgentNames.java
+++ group4/src/main/java/hotelmania/group4/HotelManiaAgentNames.java
@@ -2,8 +2,10 @@ package hotelmania.group4;
 
 /**
  * @author Marek Lewandowski <marek.lewandowski@icompass.pl>
+ * @author alberthmontero  <alberthm@gmail.com>
  * @since 20/04/14
  */
 public interface HotelManiaAgentNames {
     static final String REGISTRATION = "Registration";
+    static final String SUBSCRIBETODAYEVENT = "SubscribeToDayEvent";
 }
